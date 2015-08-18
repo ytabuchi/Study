e1e94eb191ff3d9a71b3c0ae98c930d3d13a1174
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2015-08-18 17:56:04 +0900

    INotifyPropertyChanged のテストプロジェクトを追加

---
+++ XF_INPC/XF_INPC/XF_INPC/TestPage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XF_INPC
+{
+    public class TestPage : ContentPage
+    {
+        public TestPage()
+        {
+            this.BindingContext = new TestPageViewModel();
+
+            var ed = new Editor { Text = "BindingTo TextValue" };
+            ed.SetBinding(Editor.TextProperty, "TextValue", mode: BindingMode.OneWayToSource);
+            var lb = new Label { Text = "BindingTo TextValue" };
+            lb.SetBinding(Label.TextProperty, "TextValue");
+
+            var sw = new Switch();
+            sw.SetBinding(Switch.IsToggledProperty, "BoolValue", mode: BindingMode.TwoWay);
+
+            var sc = new SwitchCell { Text = "BindingTo BoolValue" };
+            sc.SetBinding(SwitchCell.OnProperty, "BoolValue", mode: BindingMode.TwoWay);
+
+            var tc = new TextCell { Text = "BindingTo BoolValue" };
+            tc.SetBinding(TextCell.TextProperty, "BoolValue", stringFormat:"Value: {0}");
+
+            var tv = new TableView
+            {
+                Intent = TableIntent.Settings,
+                Root = new TableRoot
+                {
+                    new TableSection("TableView")
+                    {
+                        sc,
+                        tc,
+                    },
+                }
+            };
+
+            Content = new StackLayout
+            {
+                Padding = 20,
+                Children = {
+                    new Label {
+                        Text = "INotifyPropertyChanged Test",
+                        FontSize = 30,
+                    },
+                    ed,
+                    lb,
+                    sw,
+                    tv,
+                }
+            };
+        }
+    }
+}
---
+++ XF_INPC/XF_INPC/XF_INPC/TestPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace XF_INPC
+{
+    class TestPageViewModel : INotifyPropertyChanged
+    {
+        private bool _boolValue;
+
+        public bool BoolValue
+        {
+            get { return _boolValue; }
+            set
+            {
+                if (_boolValue != value)
+                {
+                    _boolValue = value;
+                    OnPropertyChanged("BoolValue");
+                }
+            }
+        }
+
+        private string _textValue;
+
+        public string TextValue {
+            get { return _textValue; }
+            set
+            {
+                if (_textValue != value)
+                {
+                    _textValue = value.ToUpper();
+                    OnPropertyChanged("TextValue");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                System.Diagnostics.Debug.WriteLine("Fired");
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
