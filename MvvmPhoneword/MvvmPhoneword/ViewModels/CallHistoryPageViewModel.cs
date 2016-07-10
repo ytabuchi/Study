f95aeadc158568f7fd62f279f6a6f78fdc4688b2
Yoshito Tabuchi <ytabuchi@xlsoft.com>
2016-07-11 00:03:45 +0900

    Add MvvmPhoneword

---
+++ MvvmPhoneword/MvvmPhoneword/ViewModels/CallHistoryPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using MvvmPhoneword.Models;
+
+namespace MvvmPhoneword.ViewModels
+{
+    public class CallHistoryPageViewModel : INotifyPropertyChanged
+    {
+        private List<NumberData> phoneNumbers;
+        public List<NumberData> PhoneNumbers
+        {
+            get { return phoneNumbers; }
+            set
+            {
+                if (phoneNumbers != value)
+                {
+                    phoneNumbers = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public CallHistoryPageViewModel()
+        {
+            this.PhoneNumbers = Numbers.Instance.PhoneNumbers;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ MvvmPhoneword/MvvmPhoneword/Views/MvvmPhonewordPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace MvvmPhoneword.Views
+{
+    public partial class MvvmPhonewordPage : ContentPage
+    {
+        public MvvmPhonewordPage()
+        {
+            InitializeComponent();
+
+
+            MessagingCenter.Subscribe<ViewModels.MvvmPhonewordPageViewModel>(
+                this,
+                "ShowCallHistoryPage",
+                async (sender) =>
+                {
+                    await Navigation.PushAsync(new CallHistoryPage());
+                }
+            );
+        }
+    }
+}
+
