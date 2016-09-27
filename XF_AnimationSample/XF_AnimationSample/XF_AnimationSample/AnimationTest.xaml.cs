31509a661c537cc46444164aa981fe5d4a71cfed
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2016-09-27 15:40:57 +0900

    Add AnimationSample Project

---
+++ XF_AnimationSample/XF_AnimationSample/XF_AnimationSample/AnimationTest.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XF_AnimationSample
+{
+    public partial class AnimationTest : ContentPage
+    {
+        Dictionary<string, Easing> animations = new Dictionary<string, Easing>
+        {
+            { "BounceIn", Easing.BounceIn },
+            { "BounceOut", Easing.BounceOut },
+            { "CubicIn", Easing.CubicIn },
+            { "CubicInOut", Easing.CubicInOut },
+            { "CubicOut", Easing.CubicOut },
+            { "Linear", Easing.Linear },
+            { "SinIn", Easing.SinIn },
+            { "SinInOut", Easing.SinInOut },
+            { "SinOut", Easing.SinOut },
+            { "SpringIn", Easing.SpringIn },
+            { "SpringOut", Easing.SpringOut }
+        };
+
+        Easing easing;
+
+        public AnimationTest()
+        {
+            InitializeComponent();
+
+            // See the more detailed sample in https://developer.xamarin.com/api/type/Xamarin.Forms.Picker/
+            easingPicker.SelectedIndexChanged += (sender, e) =>
+            {
+                if (easingPicker.SelectedIndex == -1)
+                {
+                    easing = Easing.Linear;
+                    button.IsEnabled = true;
+                }
+                else
+                {
+                    // Get the item name from `SelectedIndex`. Then map `Easing` enum using "animations" dictionary.
+                    string easingName = easingPicker.Items[easingPicker.SelectedIndex];
+                    easing = animations[easingName];
+                    button.IsEnabled = true;
+                }
+            };
+
+            button.Clicked += async (sender, e) =>
+            {
+                double d1, d2;
+                int i;
+                if (double.TryParse(x.Text, out d1) && double.TryParse(y.Text, out d2) && int.TryParse(length.Text, out i))
+                {
+                    // Animate BoxView.
+                    await boxView.TranslateTo(d1, d2, (uint)i, easing);
+                }
+            };
+        }
+
+        /// <summary>
+        /// Set this page as a root page.
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Navigation.RemovePage(Navigation.NavigationStack[0]);
+        }
+    }
+}
---
+++ XF_AnimationSample/XF_AnimationSample/XF_AnimationSample/SplashPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XF_AnimationSample
+{
+    public class SplashPage : ContentPage
+    {
+        Image iconImage;
+
+        public SplashPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var abs = new AbsoluteLayout();
+
+            iconImage = new Image
+            {
+                Source = "Twitter.png",
+                WidthRequest = 100,
+                HeightRequest = 100
+            };
+            AbsoluteLayout.SetLayoutFlags(iconImage,
+                AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(iconImage,
+                new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            abs.Children.Add(iconImage);
+
+            this.BackgroundColor = Color.FromHex("#429de3");
+            this.Content = abs;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await iconImage.ScaleTo(1, 2000); //初期化などの時間のかかる処理
+            await iconImage.ScaleTo(0.9, 1500, Easing.Linear);
+            await iconImage.ScaleTo(150, 1200, Easing.Linear);
+            await Navigation.PushAsync(new AnimationTest());
+        }
+    }
+}
