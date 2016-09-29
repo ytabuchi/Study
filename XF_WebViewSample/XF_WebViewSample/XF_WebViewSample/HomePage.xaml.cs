90fa94fd4adaa731f1250cb80130ce76ab4d49ac
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2016-09-29 16:27:40 +0900

    Add WebViewSample

---
+++ XF_WebViewSample/XF_WebViewSample/XF_WebViewSample/HomePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XF_WebViewSample
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClick(object sender, EventArgs s)
+        {
+            if (entry.Text != null)
+            await Navigation.PushAsync(new WebViewPage(entry.Text));
+        }
+    }
+}
---
+++ XF_WebViewSample/XF_WebViewSample/XF_WebViewSample/WebViewPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XF_WebViewSample
+{
+    public partial class WebViewPage : ContentPage
+    {
+        public WebViewPage(string url)
+        {
+            InitializeComponent();
+
+            browser.Source = url;
+        }
+
+        void BackClicked(object sender, EventArgs s)
+        {
+            if (browser.CanGoBack)
+                browser.GoBack();
+        }
+
+        void ForwardClicked(object sender, EventArgs s)
+        {
+            if (browser.CanGoForward)
+                browser.GoForward();
+        }
+
+        void GoClicked(object sender, EventArgs s)
+        {
+            if (urlEntry.Text != null && urlEntry.Text.Contains("http"))
+            {
+                browser.Source = urlEntry.Text;
+            }
+        }
+
+        void WebOnNavigating(object sender, WebNavigatingEventArgs s)
+        {
+            loading.IsVisible = true;
+        }
+
+        void WebOnNavigated(object sender, WebNavigationEventArgs s)
+        {
+            loading.IsVisible = false;
+            urlEntry.Text = s.Url;
+        }
+    }
+}
