927c0423448d93380d6a102c6497d79871816a03
Yoshito Tabuchi <ytabuchi@xlsoft.com>
2017-02-09 18:49:09 +0900

    Add Xamarin.Forms Share project
    just implement to android

---
+++ XFShareSample/Droid/IntentRecieverActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace XFShareSample.Droid
+{
+    [Activity(Label = "XFShareSampleで共有")]
+    [IntentFilter(new[] { Intent.ActionSend },
+                  Categories = new[] { Intent.CategoryDefault },
+                  DataMimeType = "text/plain")]
+    [IntentFilter(new[] { Intent.ActionSend },
+                  Categories = new[] { Intent.CategoryDefault },
+                  DataMimeType = "image/*")]
+    
+    public class IntentRecieverActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Intent.Action == Intent.ActionSend)
+            {
+                if (Intent.Type.Contains("text/"))
+                {
+                    //テキスト処理
+                    foreach (var key in Intent.Extras.KeySet())
+                    {
+                        System.Diagnostics.Debug.WriteLine($"【KEY】{key}\n【VALUE】{Intent.Extras.Get(key)}");
+                    }
+                    Toast.MakeText(this, Intent.Extras.Get(Intent.ExtraText).ToString(), ToastLength.Short).Show();
+                }
+                else if (Intent.Type.Contains("image/"))
+                {
+                    //画像処理
+                    var stream = Intent.Extras.Get(Intent.ExtraStream);
+                    System.Diagnostics.Debug.WriteLine(stream);
+                    Toast.MakeText(this, stream.ToString(), ToastLength.Long).Show();
+                }
+
+                var intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            }
+        }
+    }
+}
---
+++ XFShareSample/XFShareSample/XFShareSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFShareSample
+{
+    public partial class XFShareSamplePage : ContentPage
+    {
+        public XFShareSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
